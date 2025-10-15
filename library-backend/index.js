88c57cba81ca2575068eb817de321e2fc415b7c4
Miqueas <miqueasgigena18@gmail.com>
2025-10-15 13:27:28 -0300

    Exercise 8.14

--- library-backend/index.js
+++ library-backend/index.js
@@ -7,7 +7,7 @@ const { GraphQLError } = require('graphql')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const Author = require('./models/author')
-
+const Book = require('./models/book')
 
 const MONGODB_URI = process.env.MONGODB_URI
 
@@ -21,111 +21,11 @@ mongoose.connect(MONGODB_URI)
     console.log('error connection to MongoDB:', error.message)
   })
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
-function countBooksAuthor(name, books){
-  let count = 0
-  for (let i = 0; i < books.length; i++) {
-    if (books[i].author === name) {
-      count = count + 1
-    }
-  }
-  return count
-}
-
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-    bookCount: countBooksAuthor('Robert Martin', books)
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-    bookCount: countBooksAuthor('Martin Fowler', books)
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-    bookCount: countBooksAuthor('Fyodor Dostoevsky', books)
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    bookCount: countBooksAuthor('Joshua Kerievsky', books)
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    bookCount: countBooksAuthor('Sandi Metz', books)
-  },
-]
-
-/*
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-*/
-
 const typeDefs = `
   type Author {
     name: String!
     id: ID!
     born: Int
-    bookCount: Int!
   }
 
   type Book {
@@ -142,7 +42,7 @@ const typeDefs = `
       author: String!
       published: Int!
       genres: [String!]!
-    ): Book
+    ): Book!
 
     editAuthor(name: String!, setBornTo: Int!): Author
   }
@@ -157,65 +57,52 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
+    bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
-    allBooks: (roots, args) => {
-      let result = books
-    
+    allBooks: async (roots, args) => {
+      let filter = {}
       if (args.author) {
-        result = books.filter(b => b.author === args.author)
-      }
-      if (args.genre) {
-        result = books.filter(b => b.genres.some(g => args.genre.includes(g)))
-      }
-      if (args.author && args.genre) {
-        result = books.filter(b => (b.author === args.author) && (b.genres.some(g => args.genre.includes(g))))
+        const author = await Author.findOne({ name: args.name })
+        if (author) {
+          filter.author = author._id
+          console.log('filter.author', filter.author)
+        } else {
+          return []
+        }
       }
 
-      return result
+      if (args.genre){
+        filter.genres = { $in: args.genre }
+        console.log('filter.genres', filter.genres)
+      }
+
+      return Book.find(filter).populate('author')
     },
     allAuthors: (roots, args) => authors
   },
 
   Mutation: {
-    addBook: (roots, args) => {
-      if (books.find(b => b.title === args.title)) {
-        throw new GraphQLError('Title must be unique', {
+    addBook: async (roots, args) => {
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
+        author = new Author({ name: args.author })
+        await author.save()
+      }
+      const book = new Book({ ...args, author: author._id })
+
+      try {
+        await book.save()
+      }
+      catch(error) {
+        throw new GraphQLError('Saving book failed', {
           extensions: {
             code: 'BAD_USER_INPUT',
-            invalidArgs: args.title
+            invalidArgs: args.title,
+            error
           }
         })
       }
-
-      const book = { ...args, id: uuid()}
-
-      // if author exists, find it's index and update it's bookCount property
-      const index = authors.findIndex(a => a.name === book.author)
-
-      if (index !== -1) {
-        authors[index].bookCount += 1
-      }
-
-      // if author doesn't exist, create a new one
-      if (index === -1) {
-        let newAuthor = {
-          name: book.author,
-          born: null,
-          id: uuid(),
-          bookCount: 1
-        }
-        authors = authors.concat(newAuthor)
-      }
-
-      books = books.concat(book)
-
-      console.log('BOOKS', books)
-      console.log('AUTHORS', authors)
-      console.log('books Martin Fowler:', countBooksAuthor('Martin Fowler', books))
-      console.log('books Reijo Mäki:', countBooksAuthor('Reijo Mäki', books))
-
-      return book
+      return book.populate('author')
     },
 
     editAuthor: (roots, args) => {
